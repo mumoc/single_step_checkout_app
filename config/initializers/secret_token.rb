14b47ea7563ea837d29352f323bcd38292ddfea7
Mumo Carlos <mumo.crls@gmail.com>
2014-06-16 18:14:37 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SSCO::Application.config.secret_key_base = '97112f2f137be8e85f7dcb1fc348f5f2df2e156c2943f345cada076a83f29217c491d921c343497aa7e3c852dc3cb5f76183b5260bd93ed1dda27524d7a87c50'
